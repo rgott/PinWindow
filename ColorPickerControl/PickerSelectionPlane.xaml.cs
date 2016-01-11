65a91cbadf41641f0256a1512a535070c1d21c98
Rob Gott <robgott@yahoo.com>
2016-01-11 01:25:24 -0500

    Made additions to the color selector and improved functionality

--- ColorPickerControl/PickerSelectionPlane.xaml.cs
+++ ColorPickerControl/PickerSelectionPlane.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,14 +22,26 @@ namespace Pin.ColorPicker
     /// </summary>
     public partial class PickerSelectionPlane : UserControl
     {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        static extern Int32 ReleaseDC(IntPtr hwnd, IntPtr hdc);
+        [DllImport("gdi32.dll")]
+
+
+        static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+        IntPtr hdc;
         public PickerSelectionPlane()
         {
             InitializeComponent();
+            
         }
          
         private void MajorColorSelectorPlane_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            MajorColorSelector.Margin = new Thickness(0, e.GetPosition(MajorColorSelectorPlane).Y, 0, 0);
+            MajorColorSelection(e.GetPosition(MajorColorSelectorPlane));
         }
         
         private void MajorColorSelectorPlane_MouseUp(object sender, MouseButtonEventArgs e)
@@ -44,8 +57,39 @@ namespace Pin.ColorPicker
         {
             if(Mouse.LeftButton == MouseButtonState.Pressed)
             {
-                MajorColorSelector.Margin = new Thickness(0, e.GetPosition(MajorColorSelectorPlane).Y, 0, 0);
+                MajorColorSelection(e.GetPosition(MajorColorSelectorPlane));
             }
+        }
+
+        private void MajorColorSelection(Point point)
+        {
+            MajorColorSelector.Margin = new Thickness(0, point.Y, 0, 0);
+            Point screenPoint = MajorColorSelectorPlane.PointToScreen(point);
+            IntPtr hdc = GetDC(IntPtr.Zero);
+            uint color = GetPixel(hdc, (int)screenPoint.X, (int)screenPoint.Y);
+            ReleaseDC(IntPtr.Zero, hdc);
+            byte a = (byte)(color >> 24);
+            byte r = (byte)(color >> 16);
+            byte g = (byte)(color >> 8);
+            byte b = (byte)(color >> 0);
+            PrimaryFillColor.Fill = new SolidColorBrush(Color.FromArgb(255, r, g, b));
+        }
+
+        private void ColorSelectionGrid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                Point mousPos = e.GetPosition(ColorSelectionGrid);
+                ColorSelectionGridColorFinder.Margin = new Thickness(mousPos.X - 5, mousPos.Y - 5, 0, 0);
+            }
+
+        }
+
+        private void ColorSelectionGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+                Point mousPos = e.GetPosition(ColorSelectionGrid);
+                ColorSelectionGridColorFinder.Margin = new Thickness(mousPos.X - 5, mousPos.Y - 5, 0, 0);
         }
     }
 }
