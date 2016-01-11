23a452d1a870d9a3c519797bcdaf04a394d41ac2
Rob Gott <robgott@yahoo.com>
2016-01-10 21:37:56 -0500

    Initial

---
+++ Pin/ClassExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin
+{
+    static class ClassExtensions
+    {
+        public static bool contains(this string[] strCollection, string str)
+        {
+            foreach (string item in strCollection)
+            {
+                if(str.Equals(item))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
