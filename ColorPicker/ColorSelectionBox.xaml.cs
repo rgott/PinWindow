366eaf83cf64828fea9aa8beae4d25323725acc4
Robert Gott <robgott@yahoo.com>
2017-01-15 13:53:57 -0500

    project saves Model.Project color picker cleaned up to work with project

---
+++ ColorPicker/ColorSelectionBox.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pin.ColorPicker
+{
+
+    /// <summary>
+    /// Interaction logic for ColorSelectionBox.xaml
+    /// </summary>
+    public partial class ColorSelectionBox : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private Brush _FillColor;
+        public Brush FillColor
+        {
+            get
+            {
+                return _FillColor;
+            }
+            set
+            {
+                _FillColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+
+        public ColorSelectionBox()
+        {
+            FillColor = new SolidColorBrush(Colors.Red);
+            DataContext = this;
+            InitializeComponent();
+            FillColor = UI_PickerSelectionPlane.FillColor;
+            MainWindow.MinimizedWindow += MainWindow_MinimizedWindow;
+        }
+
+        private void MainWindow_MinimizedWindow(EventArgs e)
+        {
+            Dispatcher.Invoke(() => { UI_Popup_PickerPlane.IsOpen = false; });
+        }
+
+        private void UI_Popup_PickerPlane_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MouseOverController.isMouseOverMenu = false;
+        }
+        private void UI_Btn_ProjectColor_Click(object sender, RoutedEventArgs e)
+        {
+            UI_Popup_PickerPlane.IsOpen = true;
+            MouseOverController.isMouseOverMenu = true;
+        }
+
+        private void UI_Popup_PickerPlane_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MouseOverController.isMouseOverMenu = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UI_Popup_PickerPlane.IsOpen = false;
+            FillColor = UI_PickerSelectionPlane.FillColor;
+        }
+
+    }
+}
--- Model/Project.cs
+++ Model/Project.cs
@@ -7,209 +7,31 @@ namespace Pin.Model
 {
     public class Project
     {
-        List<Item> StandardList;
-        StringCollection StandardPropertyCollection = new StringCollection();
-        private bool changeDetected = false;
-        int idCounter = 0;
+        public string ProjectName { get; set; }
+        public string ProjectPath { get; set; }
+        public Brush Color { get; set; }
 
-        public Project(StringCollection savePropertyCollection) : this()
+        public Project(string ProjectName,string ProjectPath,Brush Color)
         {
-            load(savePropertyCollection);
+            this.ProjectName = ProjectName;
+            this.ProjectPath = ProjectPath;
+            this.Color = Color;
         }
 
-        public Project()
+
+
+        public override bool Equals(object obj)
         {
-            StandardList = new List<Item>();
+            if (obj is string)
+            {
+                return ProjectName.Equals(obj as string);
+            }
+            else if (obj is Project)
+            {
+                return ProjectName.Equals((obj as Project).ProjectName);
+            }
+            return false;
         }
 
-        public void add(Color color, string Name, string Directory, bool isFavorite, bool isPinned, Item.Operation operations)
-        {
-            StandardList.Add(new Project.Item(idCounter++,color,Name,Directory,isFavorite,isPinned,operations));
-            changeDetected = true;
-        }
-
-        public void add(int id,String loadString)
-        {
-            StandardList.Add(new Project.Item(loadString));
-            changeDetected = true;
-        }
-
-        public void load()
-        {
-            load(StandardPropertyCollection);
-        }
-        public void load(StringCollection savePropertyCollection)
-        {
-            foreach (string item in savePropertyCollection)
-            {
-                add(idCounter++,item);
-            }
-        }
-
-        public void save(StringCollection savePropertyCollection)
-        {
-            //foreach (Item item in StandardList)
-                //if (item.changeDetected && StandardPropertyCollection.(item.id))
-                //{
-                    
-                //}
-
-                foreach (Item item in StandardList)
-                StandardPropertyCollection.Add(item.saveValue());
-            //Settings.saveProjects(StandardPropertyCollection);
-            
-        }
-        public class Item
-        {
-            #region Declarations
-            public readonly int id;
-            private Color itemColor;
-            public Color ItemColor
-            {
-                get
-                {
-                    return itemColor;
-                }
-                set
-                {
-                    changeDetected = true;
-                    ItemColor = value;
-                }
-            }
-            private string name;
-            public string Name
-            {
-                get
-                {
-                    return name;
-                }
-                set
-                {
-                    changeDetected = true;
-                    name = value;
-                }
-            }
-            private string directory;
-            public string Directory
-            {
-                get
-                {
-                    return directory;
-                }
-                set
-                {
-                    changeDetected = true;
-                    directory = value;
-                }
-            }
-            private bool isFavorite;
-            public bool IsFavorite
-            {
-                get
-                {
-                    return isFavorite;
-                }
-                set
-                {
-                    changeDetected = true;
-                    isFavorite = value;
-                }
-            }
-            private bool isPinned;
-            public bool IsPinned
-            {
-                get
-                {
-                    return isPinned;
-                }
-                set
-                {
-                    changeDetected = true;
-                    isPinned = value;
-                }
-            }
-            public static char separator = (char)31; // INFORMATION SEPARATOR ONE
-            private Operation operation;
-            public Operation Operations
-            {
-                get
-                {
-                    return operation;
-                }
-                set
-                {
-                    changeDetected = true;
-                    operation = value;
-                }
-            }
-            public bool changeDetected = false;
-            #endregion
-            public enum Operation
-            {
-                Move,
-                Copy
-            }
-            public Item()
-            {
-
-            }
-
-            /// <summary>
-            /// Loads project item from string
-            /// </summary>
-            /// <param name="loadString"></param>
-            public Item(String loadString)
-            {
-                // parse the project item string
-                String[] properties = loadString.Split(separator);
-                try
-                {
-                    int i = 0;
-                    itemColor = (Color)ColorConverter.ConvertFromString(properties[i++]);
-                    Name = properties[i++];
-                    Directory = properties[i++];
-                    isFavorite = Boolean.Parse(properties[i++]);
-                    isPinned = Boolean.Parse(properties[i++]);
-                    operation = ParseEnum<Operation>(properties[i++]);
-                }
-                catch (Exception)
-                {
-                    // problem with properties
-                }
-
-            }
-
-            public Item(int id, Color color, string Name, string Directory, bool isFavorite, bool isPinned, Operation operations)
-            {
-                this.id = id;
-                this.itemColor = color;
-                this.Name = Name;
-                this.Directory = Directory;
-                this.isFavorite = isFavorite;
-                this.isPinned = isPinned;
-                this.operation = operations;
-            }
-
-            public override string ToString()
-            {
-                return itemColor.ToString() + separator
-                     + Name + separator
-                     + Directory + separator
-                     + isFavorite + separator
-                     + isPinned;
-            }
-            public string saveValue()
-            {
-                return ToString();
-            }
-            public static T ParseEnum<T>(string value)
-            {
-                return (T)Enum.Parse(typeof(T), value, true);
-            }
-        }
     }
-
-
-
-
 }
